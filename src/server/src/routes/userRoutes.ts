43e34053ff6c81c1c4adc47789e99c340a4c132f
Li4o <ryoji1024t@gmail.com>
2026-07-29 21:12:26 +0900

    update userRoutes.ts: add createUser

--- src/server/src/routes/userRoutes.ts
+++ src/server/src/routes/userRoutes.ts
@@ -1,9 +1,12 @@
 import { Router }  from 'express';
-import { getUsers } from '../controllers/userController.js';
+import { createUser, getUsers } from '../controllers/userController.js';
 
 const router = Router();
 
 // GET /api/v1/users
 router.get('/', getUsers);
+
+// POST /api/v1/users
+router.post('/', createUser);
 
 export default router;
