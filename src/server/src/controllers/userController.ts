65beaab86cc28ece685ccfae13ba3458ed6bf3d5
Li4o <ryoji1024t@gmail.com>
2026-07-30 12:10:34 +0900

    update userController.ts: add custom fields CRUD

--- src/server/src/controllers/userController.ts
+++ src/server/src/controllers/userController.ts
@@ -2,6 +2,11 @@ import type { Request, Response } from 'express';
 import bcrypt from 'bcryptjs';
 import { prisma } from '../lib/prisma.js';
 
+// Item
+
+// Asset Logs
+
+// Users
 // GET: Fetch all users
 export const getUsers = async (req: Request, res: Response) => {
     try {
@@ -55,8 +60,62 @@ export const createUser = async (req: Request, res: Response) => {
     }
 };
 
+// Custom Field
+// Get: Fetch all custom fields
+export const getCustomFields = async (req:Request, res:Response) => {
+    try {
+        // Fetch custom fields from PostgreSQL
+        const customFields = await prisma.customFields.findMany();
+
+        res.status(200).json({ success: true, data: customFields});
+
+    } catch (error) {
+        console.error('❌ Error fetching custom fields:', error);
+        res.status(500).json({ success: false, error: String(error) });
+    }
+}
+
+// POST: Create a new custom field
+export const createCustomField = async (req: Request, res: Response) => {
+    try {
+        const { name, type, options, createdBy } = req.body;
+
+        const newCustomField = await prisma.customFields.create({
+            data: {
+                name,
+                type,
+                options,
+                createdBy,
+            },
+        });
+
+        res.status(201).json({ success: true, data: newCustomField });
+
+    } catch (error) {
+        console.error('❌ Error creating custom field:', error);
+        res.status(500).json({ success: false, error: String(error) });  
+    }
+}
+
+// DELETE: Delete a custom field
+export const deleteCustomField = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+
+        const deletedCustomField = await prisma.customFields.delete({
+            where: { id },
+        });
+
+        res.status(203).json({ success: true, data: deletedCustomField });
+
+    } catch (error) {
+        console.error('❌ Error deleting custom field:', error);
+        res.status(500).json({ success: false, error: String(error) });  
+    }
+}
+
 // Class
-// GET: List classes
+// GET: Fetch all classes
 export const getClasses = async (req: Request, res: Response) => {
     try {
         // Fetch classes from PostgreSQL
@@ -85,7 +144,7 @@ export const createClass = async (req: Request, res: Response) => {
         res.status(201).json({ success: true, data: newClass });
         
     } catch (error) {
-        console.error('❌ Error fetching classes:', error);
+        console.error('❌ Error creating classes:', error);
         res.status(500).json({ succuess: false, error: String(error) });
     }
 };
