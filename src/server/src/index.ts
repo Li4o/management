a00f3215dc56d005dd563f6d38d317da680d10bc
Li4o <ryoji1024t@gmail.com>
2026-07-30 10:44:35 +0900

    update index.ts: add classRoutes

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js'
+import classRoutes from './routes/classRoutes.js'
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ app.use(express.json());
 
 // Mount User Routes
 app.use('/api/v1/users', userRoutes);
+// Mount Class Routes
+app.use('/api/v1/classes', classRoutes);
 
 app.listen(PORT, () => {
     console.log(`? Server running on http://localhost:${PORT}`);
